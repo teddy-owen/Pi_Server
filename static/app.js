cdead7f831253266eb522bf5adeb098a7d9e0be1
Teddy Owen <david.e.owen1@gmail.com>
2018-08-20 15:43:58 -0400

    add post func

--- static/app.js
+++ static/app.js
@@ -1,15 +1,19 @@
 let cameraOn = false;
 
-let camReq = new XMLHttpRequest();
-camReq.open("GET", "/cam", true);
 
-camReq.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-	  console.log(this.responseText);
-	  cameraOn = !cameraOn;
-	  let active = cameraOn ? "active":"";
-	  document.querySelector(".cam").classList.add(active);
-	}
+
+const toggleCamera = () => {
+	let camReq = new XMLHttpRequest();
+	camReq.open("GET", "/cam", true);
+
+	camReq.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+		  console.log(this.responseText);
+		  cameraOn = !cameraOn;
+		  let active = cameraOn ? "active":"";
+		  document.querySelector(".cam").classList.add(active);
+		}
+	};
+	
+	camReq.send();
 };
-
-const toggleCamera = () => camReq.send();
