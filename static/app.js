785dea357a5d7791a5be0703d0cc9aefb6d19d50
Teddy Owen <david.e.owen1@gmail.com>
2018-08-20 15:37:55 -0400

    add post func

--- static/app.js
+++ static/app.js
@@ -1,4 +1,10 @@
 let camReq = new XMLHttpRequest();
 camReq.open("GET", "/cam", true);
 
-const toggleCamera = () => camReq.send().then(resp => console.log(resp));
+camReq.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+	  console.log(this.responseText);
+	}
+};
+
+const toggleCamera = () => camReq.send();
