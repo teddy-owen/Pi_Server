61e745082ddc18c00ffb1f2cf5c97f1895915a93
Teddy Owen <david.e.owen1@gmail.com>
2018-08-20 15:42:13 -0400

    add post func

--- static/app.js
+++ static/app.js
@@ -1,9 +1,14 @@
+let cameraOn = false;
+
 let camReq = new XMLHttpRequest();
 camReq.open("GET", "/cam", true);
 
 camReq.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
 	  console.log(this.responseText);
+	  cameraOn = !cameraOn;
+	  let active = cameraOn ? "active":"";
+	  document.querySelector(".cam").classList.add(active);
 	}
 };
 
